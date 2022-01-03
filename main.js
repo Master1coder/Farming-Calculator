de7a00d70145f1844e67c242e036c84d34178ec7
Master1coder <79441875+Master1coder@users.noreply.github.com>
2022-01-03 15:51:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function calculate(){
+length=document.getElementById("l").value;
+width=document.getElementById("w").value;
+area=length*width;
+document.getElementById("number1").innerHTML=area;
+}
